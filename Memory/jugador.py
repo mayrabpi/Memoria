f298049c5a1518a2392850462d8d5595f2b7a132
mayrabpi <145108717+mayrabpi@users.noreply.github.com>
2024-11-03 14:22:30 +0100

    coment

--- Memory/jugador.py
+++ Memory/jugador.py
@@ -1,4 +1,6 @@
 class Jugador:
+    """Clase que represnta a un jugador en el juego de memoria.
+    Mantiene el resgitro de su número, nombre y puntuación"""
     def __init__(self,numero):
         self.numero = numero
         self.puntos=0
